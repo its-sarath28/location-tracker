a39e6a6ee7f987655da53cb7078b031ef03a2813
Sarath R S <78993686+its-sarath28@users.noreply.github.com>
2024-06-17 03:19:36 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "https://location-tracker-six.vercel.app",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST"],
   },
 });
